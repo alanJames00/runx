ecb99c3af9864de0b7e7e9f7893416797e1739ae
alan <alanjames6282@gmail.com>
2024-01-14 18:40:17 +0530

    add routes to list and exec

--- api/routes.js
+++ api/routes.js
@@ -9,6 +9,34 @@ apiRouter.get('/t', (req, res) => {
 });
 
 
+apiRouter.get('/runtimes', async (req, res) => {
+  try {
+    const result = [{
+      runtime: 'javascript/nodejs',
+      alias: 'js',
+      version: 'lts'
+    }];
+
+    res.json(result);
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+})
+
+apiRouter.post('/execute', async (req, res) => {
+  try {
+    
+    // parse the code and runtime
+    const codeString = req.body.code;
+    const runtime = req.body.runtime;
+  }
+  catch(e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+})
 
 // export the apiRouter
 module.exports = apiRouter;
