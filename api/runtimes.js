b8ef368cb7b75150dc2d69d3f1d08fbc0fc8658a
alan <alanjames6282@gmail.com>
2024-02-22 16:25:21 +0530

    add runtimes list as array

---
+++ api/runtimes.js
@@ -0,0 +1,13 @@
+module.exports.runtimes = [{
+    language: 'C++',
+    runtime_alias: 'cpp',
+},
+{
+    language: 'C',
+    runtime_alias: 'c'
+},
+{
+    language: 'Nodejs',
+    runtime_alias: 'js'
+}
+]
