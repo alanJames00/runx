7df5d73f5dd70dcdb6532807f5c624a6dd11d281
alan <alanjames6282@gmail.com>
2024-04-05 01:09:43 +0530

    complete execute function with container cleanup

--- exec/execute.js
+++ exec/execute.js
@@ -1,6 +1,7 @@
 const docker = require('./docker');
 const fs = require('fs');
 const uuid = require('uuid');
+const counter = require('../api/counter');
 
 // execute the code
 async function executeCode({codeString, runtime}) {
@@ -25,8 +26,37 @@ async function executeCode({codeString, runtime}) {
         }
         console.log(codeString);
         // create  the file
-        fs.writeFileSync(`./exec/tmp/${filename}`, codeString, 'utf-8'); 
+        fs.writeFileSync(`./exec/tmp/${filename}`, codeString, 'utf-8');
         
+        
+        // create a container
+        const containerName = "runx-pod-"+counter.getContainerCount().toString();
+        const createRes = await docker.createContainer({ containerName: containerName });
+        console.log(createRes); 
+        
+        // increment the counter
+        counter.incrementContainerCount();
+
+        // copy the file to container
+        const putFileRes = await docker.putFileInContainer(containerName, `./exec/tmp/${filename}`)
+        console.log(putFileRes);
+
+        // start the container
+        const startRes = await docker.startContainer(containerName);
+        console.log(startRes);
+
+        // exec the container to echo the file
+        const execRes = await docker.execCommand(containerName, `cat /tmp/${filename}`);
+        console.log(execRes);
+
+        // cleanup containers
+        const cleanUpRes = await docker.cleanUp(containerName);
+        console.log(cleanUpRes);
+        
+        // remove the file
+        await fs.rmSync(`./exec/tmp/${filename}`);
+
+        return execRes;
     }
     catch(e) {
         console.log(e);
