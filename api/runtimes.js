2bf89aa8a79498bbcfaf483ab34ab7bb14522e93
alan <alanjames6282@gmail.com>
2024-04-06 23:57:16 +0530

    add version info to runtimes

--- api/runtimes.js
+++ api/runtimes.js
@@ -1,13 +1,20 @@
 module.exports.runtimes = [{
     language: 'C++',
     runtime_alias: 'cpp',
+    version: 'g++ v9.4.0'
 },
 {
     language: 'C',
-    runtime_alias: 'c'
+    runtime_alias: 'c',
+    version: 'gcc v9.4.0'
 },
 {
     language: 'Nodejs',
-    runtime_alias: 'js'
+    runtime_alias: 'js',
+    version: '18.20.1'
+},
+{
+    language: 'Python3',
+    runtime_alias: 'py3'
 }
 ]
