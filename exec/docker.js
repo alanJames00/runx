43348cf1e6647cb25e439ad4b038da6a85b0822e
alan <alanjames6282@gmail.com>
2024-04-06 15:44:15 +0530

    change all docker functions to sync

--- exec/docker.js
+++ exec/docker.js
@@ -2,36 +2,41 @@ const childprocess = require('child_process');
 const fs = require('fs');
 
 // list all stopped containers in an aync function
-async function listStoppedContainers() {
+function listStoppedContainers() {
 
     const containers = childprocess.execSync('docker ps -a').toString().split('\n');
     return containers;
 }
 
-async function createContainer({ containerName }) {
-    const container = childprocess.execSync(`docker create -it --name ${containerName} alpine:latest`).toString();
+function createContainer({ containerName }) {
+    const container = childprocess.execSync(`docker create -it --name ${containerName} runx:latest`).toString();
     return container;
 }
 
-async function putFileInContainer(containerId, file) {
-    const container = childprocess.execSync(`docker cp ${file} ${containerId}:/tmp`).toString();
+function putFileInContainer(containerId, file) {
+    const container = childprocess.execSync(`docker cp ${file} ${containerId}:/tmp/`).toString();
     return container;
 }
 
-async function startContainer(containerId) {
+function startContainer(containerId) {
     const container = childprocess.execSync(`docker start ${containerId}`).toString();
     return container;
 }
 
-async function execCommand(containerId, command) {
+function execCommand(containerId, command) {
     const container = childprocess.execSync(`docker exec ${containerId} ${command}`).toString();
     return container;
 }
 
-async function cleanUp(containerId) {
+
+function cleanUp(containerId) {
+    
+    const exitCode = childprocess.execSync(`docker inspect -f '{{.State.ExitCode}}' ${containerId}`).toString();
+
     const coinatiner = childprocess.execSync(`docker kill ${containerId}`).toString();
     const container2 = childprocess.execSync(`docker rm ${containerId}`).toString();
-    return container2;
+    
+    return exitCode;
 }
 
 
