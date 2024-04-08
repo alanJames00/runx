8e2aaf1691055f2cc81fee0b85e0cf87649a6d52
alan <alanjames6282@gmail.com>
2024-04-08 22:36:33 +0530

    clean code and add docker image builders

--- docker-image/ex.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream> 
-using namespace std;
-int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    cout<<arr[6];
-    return 0;
-}
-
--- docker-image/ex.java
+++
@@ -1,6 +0,0 @@
-public class ex {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
